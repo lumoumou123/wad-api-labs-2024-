4a7e9083c64d3129029098d222e2602f1ef0e42c
lumoumou123 <20109114@mail.wit.ie>
2024-12-03 12:51:50 +0000

    added routing for posting a new task

--- tasky-api/api/tasks/index.js
+++ tasky-api/api/tasks/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { v4 as uuidv4 } from 'uuid';
 import { tasksData } from './tasksData';
 
 const router = express.Router(); 
@@ -14,5 +15,19 @@ router.get('/:id', (req, res) => {
         return res.status(404).json({ status: 404, message: 'Task not found' });
     }
     return res.status(200).json(task);
+});
+router.post('/', (req, res) => {
+    const { title, description, deadline, priority, done } = req.body;
+    const newTask = {
+        id: uuidv4(),
+        title,
+        description,
+        deadline,
+        priority,
+        done
+    };
+    tasksData.tasks.push(newTask);
+    res.status(201).json(newTask);
+    tasksData.total_results++;
 });
 export default router;
